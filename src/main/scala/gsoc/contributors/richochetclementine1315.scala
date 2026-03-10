4324092e04b96ec0ba082ce65e121e0bb78636f3
Mrinmoy_1315 <mrinmoymatilal1315@gmail.com>
2026-03-10 13:55:35 +0530

    Statement for TypeLevel code of conduct and gsoc Ai policy
    Signed-off-by: Mrinmoy_1315 <mrinmoymatilal1315@gmail.com>

--- src/main/scala/gsoc/contributors/richochetclementine1315.scala
+++ src/main/scala/gsoc/contributors/richochetclementine1315.scala
@@ -59,7 +59,7 @@ val richochetclementine1315: Contributor = Contributor("richochetclementine1315"
         text-align: center;
       """,
       h3(styleAttr := "color:#cba6f7; margin: 0 0 4px;", "🎮 Evolution Clicker"),
-      p(styleAttr := "color:#6c7086; font-size:12px; margin:0 0 16px;", "Click to evolve your creature!"),
+      p(styleAttr := "color:#6c7086; font-size:12px; margin:0 0 16px;", "Click to evolve your creature!  I agree to follow the Typelevel Code of Conduct and the GSoC AI policy"),
 
       // Big clickable emoji
       div(
